b82022ce236e16de461fed07baf334d015488ec8
Sylvain Corsini <sylvain.corsini@epitech.eu>
2017-05-26 10:25:37 +0200

    [ADD] Exemples.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-linked-list
 A generalized chained list in C easy-to-use.
+
+You have exemple of uses in exemples/ directory.
---
+++ exemples/1/main.c
@@ -0,0 +1,66 @@
+/*
+** main.c for myll in /home/sylvain/Travail/Perso/myll/exemple/main.c
+**
+** Made by Sylvain CORSINI
+** Login   <sylvain.corsini@epitech.eu>
+**
+** Started on  Fri May 26 10:16:24 2017 Sylvain CORSINI
+** Last update Fri May 26 10:16:24 2017 Sylvain CORSINI
+*/
+
+#define MYLL_TINY_DEF
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "myll.h"
+
+struct s_test
+{
+  t_myll	list;
+  int		nb;
+};
+
+typedef struct s_test t_test;
+
+int main()
+{
+  t_test	*test1;
+  t_test	*test2;
+
+  /* You have to malloc the elements. */
+  test1 = malloc(sizeof(t_test));
+  if (test1 == NULL)
+    return (EXIT_FAILURE);
+  test1->nb = 42;
+  test2 = malloc(sizeof(t_test));
+  if (test2 == NULL)
+  {
+    free(test1);
+    return (EXIT_FAILURE);
+  }
+  test2->nb = 84;
+
+  /* Call the link method on both elements. */
+  myll_link(&test1->list, &test2->list);
+
+  /* Loop over right elements. */
+  t_test	*x;
+  x = test1;
+  while (x != NULL)
+  {
+    printf("%d\n", x->nb);
+    x = (t_test *) x->list.right;
+  }
+  /* Loop over left elements. */
+  x = test2;
+  while (x != NULL)
+  {
+    printf("%d\n", x->nb);
+    x = (t_test *) x->list.left;
+  }
+
+  /* You also have to free elements. */
+  free(test1);
+  free(test2);
+  return (EXIT_SUCCESS);
+}
